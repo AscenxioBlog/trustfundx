7383da16d0f379ee63b3a602f68705da8ff352b6
Mr Blvck <complexjrilo.com>
2025-02-19 20:16:02 +0100

    Mr Blvck commit

--- src/UI/HomeComponent/HomePage.jsx
+++ src/UI/HomeComponent/HomePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div className=' h-[100vh] w-full bg-amber-600'>
-      
+    <div className=' h-[100vh] w-full bg-bg-color'>
+
     </div>
   )
 }
