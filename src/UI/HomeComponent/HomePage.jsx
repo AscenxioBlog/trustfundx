efc07c60f56893c0cc7d3f47e87d55706a2d8d0f
chidera-collins <chideracollins24@gmail.comm>
2025-02-19 18:56:50 +0100

    dera commit

--- src/UI/HomeComponent/HomePage.jsx
+++ src/UI/HomeComponent/HomePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import HomeComponent1 from './HomeComponent1'
+import HomeComponent2 from './HomeComponent2'
+import HomeComponent3 from './HomeComponent3'
 
 function HomePage() {
   return (
     <div className=' h-[100vh] w-full bg-mybg'>
       <HomeComponent1/>
+      <HomeComponent2/>
+      <HomeComponent3/>
       
     </div>
   )
