7c9620ebfeaaba4462a27aef0b0bbdd0d34e2dc8
Mr Blvck <complexjrilo.com>
2025-02-21 00:02:05 +0100

    New Edit

--- src/UI/AboutComponent/FAQsComponents.jsx
+++ src/UI/AboutComponent/FAQsComponents.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaPlus } from "react-icons/fa";``
 
 function FAQsComponents() {
 
@@ -7,7 +8,7 @@ function FAQsComponents() {
 
 function boy1() {
     if (account =="0px") {
-        newAccount("100px")
+        newAccount("130px")
        
 
     } else {
@@ -22,120 +23,25 @@ function boy1() {
                     <h1 className='font-medium text-4xl'>Frequently Asked Questions</h1>
                     <p className='font-medium mt-5'>Have Any Question?</p>
                     
-                    <div className='min-h-[50vh] mt-12 gap-9 mx-6 grid grid-cols-2'>
-                         <div>
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
+                    <div className='lg:min-h-[50vh] lg:w-[700px] mt-12'>
+                       <div>
+                            <div className='bg-[white] rounded overflow-hidden'>
+                                <div className='h-[70px] items-center border-b border-[gray] mx-12 font-medium flex' onClick={boy1}>
+                                   <FaPlus />
+                                    <button className='text-[#FF0000] text-[18px]'> How to Withdraw Money?</button>
                                 </div>
 
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
+                                <p className='h-[0] m-0 bg-[white] mt-3 mx-4 font-medium text-[black] text-[14px] ' style={{height:account,transition:"1s"}}>
+                                    To withdraw money from your account, you can follow these steps: 1. Log in to your account. 2. Go to the
+                                    withdrawal section. 3. Select the amount you want to withdraw (yes there’s limitations on each plan). 4. 
+                                    Choose your preferred withdrawal method. 5. Confirm the withdrawal request. 6. Wait for the processing 
+                                    and approval of your withdrawal request. 7. Once approved, the money will be transferred to your chosen 
+                                    withdrawal met                                
                                 </p>
-                            </div>
-
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-                         </div>
-
-
-
-                         <div>
-                             <div className='bg-[#000000] overflow-hidden'>
-                                    <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                        
-                                        <p> How do I create my account?</p>
-                                    </div>
-
-                                    <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                    </p>
-                            </div>
-
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-                            
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-
-                            <div className='bg-[#000000] overflow-hidden'>
-                                <div className='h-[70px] items-center font-medium flex' onClick={boy1}>
-                                       
-                                       <p> How do I create my account?</p>
-                                </div>
-
-                                <p className='font-medium h-[0] m-0 bg-[#000000] ' style={{height:account,transition:"1s"}}>
-                                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur illo debitis nulla natus ullam odio in, ipsa dicta explicabo vitae aut eum soluta sit. Distinctio, magni eius! Fuga, ducimus nisi.
-                                </p>
-                            </div>
-                         </div>
+                            </div> 
+                        </div>
                     </div>
-                <div>
-
-                </div>
-            </div>
+             </div>
 
     </div>
   )
